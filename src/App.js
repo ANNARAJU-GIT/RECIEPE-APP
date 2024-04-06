487b73d8021ee5ba63ac6e2dea424d22111eb6ed
Annaraju <annaraju311@gmail.com>
2024-04-07 03:42:19 +0530

    add reciepe completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import AddReciepe from './components/AddReciepe';
 
 function App() {
   return (
     <div>
-      <p>jjjj</p>
+      <AddReciepe/>
     </div>
   );
 }
