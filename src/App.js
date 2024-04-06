29fb09d0bf82f391f82a38816f577866052b156d
Annaraju <annaraju311@gmail.com>
2024-04-07 04:01:48 +0530

    search copleted

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import AddReciepe from './components/AddReciepe';
+import SearchReciepe from './components/SearchReciepe';
 
 function App() {
   return (
     <div>
       <AddReciepe/>
+      <SearchReciepe/>
     </div>
   );
 }
